bbfe8374574d9705c6726dcdf852357bd554f430
Ezgi Gunes <76749415+Ezgi3406@users.noreply.github.com>
2022-02-11 21:33:20 +0000

    Update members.js

--- members.js
+++ members.js
@@ -1,3 +1,46 @@
-const members = [];
+const members = [
+const members  = [
+    {
+        firsName: 'John',
+        lastName: 'Doe',
+        jobTitle: 'Software Engineer',
+        department: 'Engineering',
+        manager: 'null',
+        location: 'Glasgow',
+        firstAider: 'No',
+        userSlack: '.johndoe',
+        timeZone: 'America/Los_Angeles'
+    },
+    {
+        firsName: 'Jane',
+        lastName: 'Doe',
+        jobTitle: 'Software Engineer',
+        department: 'Engineering',
+        manager: 'John Doe',
+        location: 'Leeds',
+        firstAider: 'No',
+        userSlack: '.janedoe',
+        timeZone: 'America/Los_Angeles'
+    },
+    {
+        firsName: 'Bob',
+        lastName: 'Doe',
+        jobTitle: 'Software Engineer',
+        department: 'Engineering',
+        manager: 'Jane Doe',
+        location: 'Cape Town',
+        firstAider: 'No',
+        userSlack: '.bobdoe',
+        timeZone: 'America/Los_Angeles'
+    },
+    {
+        firsName: 'Sally',
+        lastName: 'Doe',
+        jobTitle: 'Software Engineer',
+        department: 'Engineering',
+    },
+    ];
+
 
 module.exports = members;
+
